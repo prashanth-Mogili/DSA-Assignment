afc685b9bc627e5c2356136803252987be5100bf
Mogili Prashanth Reddy <pmogili@opentext.com>
2022-08-18 20:12:25 +0530

    Initial commit

---
+++ DSA Ass-2/Prgm2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    string name;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<endl<<"Enter your age: ";
+    cin>>age;
+    cout<<endl<<"name: "<<name<<endl;
+    cout<<"age: "<<age<<endl;
+
+    return 0;
+}
